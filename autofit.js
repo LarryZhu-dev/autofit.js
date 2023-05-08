321bc6da0d8b49bac004e24728892b865eb76b9a
Larry <995231030@qq.com>
2023-05-08 11:25:55 +0800

    优化写法

--- autofit.js
+++ autofit.js
@@ -7,26 +7,22 @@ const autofit = {
     let designHeight = options?.designHeight || 929;
     let renderDom = options?.renderDom || "#app";
     let resize = options?.resize || true;
-    document.querySelector(renderDom).style.height = `${designHeight}px`;
-    document.querySelector(renderDom).style.width = `${designWidth}px`;
-    document.querySelector(renderDom).style.transformOrigin = `0 0`;
-    keepFit(designWidth, designHeight, renderDom);
+    let dom = document.querySelector(renderDom)
+    dom.style.height = `${designHeight}px`;
+    dom.style.width = `${designWidth}px`;
+    dom.style.transformOrigin = `0 0`;
+    keepFit(designWidth, designHeight, dom);
     resize && (window.onresize = () => {
-      keepFit(designWidth, designHeight, renderDom);
+      keepFit(designWidth, designHeight, dom);
     })
   }
 }
-function keepFit(designWidth, designHeight, renderDom) {
+function keepFit(designWidth, designHeight, dom) {
   let clientHeight = document.documentElement.clientHeight;
   let clientWidth = document.documentElement.clientWidth;
-  let scale = 1;
-  if (clientWidth / clientHeight < designWidth / designHeight) {
-    scale = (clientWidth / designWidth)
-  } else {
-    scale = (clientHeight / designHeight)
-  }
-  document.querySelector(renderDom).style.height = `${clientHeight / scale}px`;
-  document.querySelector(renderDom).style.width = `${clientWidth / scale}px`;
-  document.querySelector(renderDom).style.transform = `scale(${scale})`;
+  let scale = (clientWidth / clientHeight < designWidth / designHeight) ? (clientWidth / designWidth) : (clientHeight / designHeight)
+  dom.style.height = `${clientHeight / scale}px`;
+  dom.style.width = `${clientWidth / scale}px`;
+  dom.style.transform = `scale(${scale})`;
 }
 export default autofit;
